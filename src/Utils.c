23c47677794cf15229cd6e1e4aa820e922d5e229
firewolf8385 <colbykrawczyk10@gmail.com>
2018-10-13 20:38:27 -0400

    Made Utils.c work

--- src/Utils.c
+++ src/Utils.c
@@ -7,32 +7,34 @@
 #pragma config(Motor,  port8,           DriveBR,       tmotorVex393_MC29, openLoop, driveRight)
 #pragma config(Motor,  port9,           DriveBL,       tmotorVex393_MC29, openLoop, driveLeft)
 
-task drive_foward(int time, int speed){
+void drive_foward(int time, int speed){
 	setMultipleMotors(speed, DriveFR, DriveFL, DriveBR, DriveBL);
 	wait(time);
 	setMultipleMotors(0, DriveFR, DriveFL, DriveBR, DriveBL);
 }
 
-task drive_backward(int time, int speed){
+void drive_backward(int time, int speed){
 	wait(time);
 }
 
-task lift_up(int time, int speed){
+void lift_up(int time, int speed){
 	wait(time);
 }
 
-task lift_down(int time, int speed){
+void lift_down(int time, int speed){
 	wait(time);
 }
 
-task minilift_up(int time, int speed){
+void minilift_up(int time, int speed){
 	wait(time);
 }
 
-task minilift_down(int time, int speed){
+void minilift_down(int time, int speed){
 	wait(time);
 }
 
-task ball_shoot()
-
+void ball_shoot(){
+	setMotor(BallLauncher, -127);
+	wait(1000);
+	setMotor(BallLauncher, 0);
 }
